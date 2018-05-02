10a4ba834bfd308149c293e48a2af0f9f7f8afe4
calvinlin <calvin@calvins-mbp.attlocal.net>
2018-05-01 23:45:28 -0700

    more fixes
    jquery is nice.  smooth scrolling is nicer.

---
+++ main/script.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+	init();
+});
+
+
+function init() {
+
+	/*
+	*	Add smooth scrolling to anchor tags, because the default one sucks.
+	*	src: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+	*/
+	$("a.navItem").on('click', function(event) {
+		event.preventDefault();
+	
+		var hash = this.hash;
+	
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 800, function(){
+			window.location.hash = hash;
+		});
+	});
+
+
+}
+
