6fff66a5d056c28a988e7f0c59e8131fa83d194a
calvinlin <calvin@calvins-mbp.attlocal.net>
2020-02-19 22:57:57 -0800

    updated shortcuts
    because they needed updating.

--- shortcuts/main.js
+++ shortcuts/main.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
 	$('a').each(function() {
 		$(this).attr('target', '_blank');
 	});
+
+	$('a[href*="file://"]').on('click', function(e) {
+		e.preventDefault();
+		copyText($(this).attr('href'));
+		alert('File directory has been copied to clipboard, paste in address bar of file explorer to continue.');
+	});
+
+	aprilFools();
 });
 
 //unused, maybe for future?
@@ -17,4 +25,14 @@ function copyText(str) {
 	else {
 		console.warn('empty or non-string values cannot be copied');
 	}
+}
+
+// :)
+function aprilFools() {
+	var d = new Date();
+	var mm = d.getMonth();
+	var dd = d.getDate();
+	
+	if(mm == 3 && dd == 1)
+		$('#content').css('transform', 'rotate(120deg)');
 }
