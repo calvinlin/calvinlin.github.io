acc2a5ad23bbf2d634f7efb84905c2e30ef2f52e
calvinlin <calvinlin@users.noreply.github.com>
2022-04-17 20:26:32 -0700

    updated shortcuts page

--- shortcuts/main.js
+++ shortcuts/main.js
@@ -10,13 +10,29 @@ $(document).ready(function() {
 			$('.hiddenLink').show();
 	});
 
-	$('a[href*="file://"]').on('click', function(e) {
+	//$('a[href*="file://"]').on('click', function(e) {
+	$('.copy a').on('click', function(e) {
 		e.preventDefault();
 		copyText($(this).attr('href'));
 		alert('File directory has been copied to clipboard, paste in address bar of file explorer to continue.');
 	});
 
 	fancyStuff();
+
+	////////// popup window ////////////
+	if ($(".moreproductlinks").length) {
+		if ($(window).width() > 1100) var x = "900px",
+			L = "470px";
+		else var x = "95%",
+			L = "50%";
+		$(".moreproductlinks").colorbox({
+			iframe: !0,
+			width: x,
+			height: L
+		})
+	}
+	////////// End of popup window ////////////
+
 });
 
 //unused, maybe for future?
@@ -27,7 +43,7 @@ function copyText(str) {
 		$temp.val(str).select();
 		document.execCommand("copy");
 		$temp.remove();
-	} 
+	}
 	else {
 		console.warn('empty or non-string values cannot be copied');
 	}
@@ -42,7 +58,7 @@ function fancyStuff() {
 	//st patricks day
 	if(mm == 2 && dd == 17)
 		$('#content').addClass('stpatricksday');
-	
+
 	//april fools
 	if(mm == 3 && dd == 1)
 		$('#content').css('transform', 'rotate(120deg)');
@@ -50,4 +66,7 @@ function fancyStuff() {
 	//christmas
 	if(mm == 11)
 		$('#content').addClass('christmas');
+
+	if(mm == 10)
+		$('#content').addClass('thanksgiving');
 }
